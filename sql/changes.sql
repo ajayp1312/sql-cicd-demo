c62245d361058fe421d096e5040b58ba28d599ed
ajayp1312 <ajaypatil.dt@gmail.com>
2026-07-14 12:40:36 +0000

    Initial SQL file and structure

---
+++ sql/changes.sql
@@ -0,0 +1,7 @@
+-- Example first change
+CREATE TABLE IF NOT EXISTS employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    department VARCHAR(100),
+    created_at TIMESTAMP DEFAULT NOW()
+);
